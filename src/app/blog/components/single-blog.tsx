2fcb799c2fd6b040c549330ff3cddbb21fd8102d
iamshishirgaire <shishirgaire35@gmail.com>
2024-05-06 21:38:31 +0545

    bugfix:responsive grid in singlepost page

--- src/app/blog/components/single-blog.tsx
+++ src/app/blog/components/single-blog.tsx
@@ -5,8 +5,8 @@ import { DashboardTableOfContents } from "./toc";
 
 export default function SingleBlog({ post }: { post: TPost }) {
   return (
-    <div className="grid grid-cols-[2fr_6fr_2fr] gap-8 w-full mx-auto py-12 px-4 md:px-6">
-      <div className="" />
+    <div className="grid  sm:grid-cols-1 lg:grid-cols-[2fr_6fr_2fr] gap-8 w-full mx-auto py-12 px-4 md:px-6">
+      <div />
       <div className="max-w-4xl">
         <article className="prose min-w-full  prose-gray dark:prose-invert">
           <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
@@ -16,7 +16,7 @@ export default function SingleBlog({ post }: { post: TPost }) {
           <MDXContent code={post.body} />
         </article>
       </div>
-      <div className="pr-[20px]  sticky p-10 top-[10vh]  h-fit border rounded-md">
+      <div className="pr-[20px] hidden lg:flex sticky p-10 top-[10vh]  h-fit border rounded-md">
         <DashboardTableOfContents toc={post.toc} />
       </div>
     </div>
