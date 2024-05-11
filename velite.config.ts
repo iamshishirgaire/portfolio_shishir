545e2658e76d863b2b207973f78eaf680be003d7
iamshishirgaire <shishirgaire35@gmail.com>
2024-05-11 20:07:09 +0545

    Update React 19 Beta Upgrade Guide link in blog post

--- velite.config.ts
+++ velite.config.ts
@@ -3,27 +3,27 @@ import rehypeSlug from "rehype-slug";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-const blogPosts = defineCollection({
-  name: "blogPost",
-  pattern: "blog/**/*.mdx",
-  schema: s.object({
-    slug: s.path(),
-    title: s.string(),
-    description: s.string(),
-    published: s.boolean().default(false),
-    displayImage: s.string(),
-    date: s.date(),
-    content: s.mdx(),
-    tags: s.array(s.string()),
-    category: s.string(),
-    isFeatured: s.boolean().default(false),
-    toc: s.toc(),
-  }),
-});
+// const blogPosts = defineCollection({
+//   name: "blogPost",
+//   pattern: "blog/**/*.mdx",
+//   schema: s.object({
+//     slug: s.path(),
+//     title: s.string(),
+//     description: s.string(),
+//     published: s.boolean().default(false),
+//     displayImage: s.string(),
+//     date: s.date(),
+//     content: s.mdx(),
+//     tags: s.array(s.string()),
+//     category: s.string(),
+//     isFeatured: s.boolean().default(false),
+//     toc: s.toc(),
+//   }),
+// });
 
 const computedFields = <T extends { slug: string }>(data: T) => ({
   ...data,
-  slugAsParams: data.slug.split("/").slice(1).join("/"),
+  slugAsParams: data.slug.split("/")[1].replaceAll(" ", "-"),
 });
 
 const posts = defineCollection({
