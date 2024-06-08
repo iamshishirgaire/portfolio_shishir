25cbaef1d245c822c6ae955cebeb5a6bc01abb3d
iamshishirgaire <shishirgaire35@gmail.com>
2024-06-08 10:38:21 +0545

    resume-link updated

--- src/data/links.ts
+++ src/data/links.ts
@@ -4,5 +4,5 @@ export const SocialLinks = {
   linkedin: "https://www.linkedin.com/in/shishir-gaire-157a15261/",
   twitter: "https://twitter.com/ShishirGaire5",
   resume:
-    "https://github.com/iamshishirgaire/iamshishirgaire/blob/main/resume_shsihir.pdf",
+    "https://github.com/iamshishirgaire/iamshishirgaire/blob/main/shishir-resume.pdf",
 };
