b6f53bce2b34ed778f00c18740f6c7bb71128e97
iamshishirgaire <shishirgaire35@gmail.com>
2024-08-13 09:01:48 +0545

    bugfix and refactoring : search filter

--- src/app/blog/components/search-form.tsx
+++ src/app/blog/components/search-form.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { getSearchTags } from "../repository/getSearchTags";
 import Autocomplete from "react-autocomplete";
 import { cn } from "@/lib/utils";
-import { MessageSquareText, SearchIcon } from "lucide-react";
+import { MessageSquareText, RabbitIcon, SearchIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 
 const SearchForm = () => {
@@ -12,72 +12,40 @@ const SearchForm = () => {
   const [inputValue, setInputValue] = useState("");
 
   return (
-    // <div className="relative hidden md:block w-full h-full max-w-md">
-    //   <form
-    //     onSubmit={(e) => {
-    //       e.preventDefault();
-    //       const query = new URLSearchParams();
-    //       query.set("q", inputValue);
-    //       router.push(`/blog/search?${query.toString()}`);
-    //     }}
-    //   >
-    //     {/*
-    //<SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400" />
-    //     <Input
-    //       className="w-full rounded-md  px-10 py-2 text-sm bg-popover"
-    //       placeholder="Search blog posts..."
-    //       type="search"
-    //       id="search"
-    //       name="search"
-    //       value={inputValue}
-    //       onChange={(e) => setInputValue(e.target.value)}
-    //     />
-    //     <div className="absolute mt-3">{}</div> */}
-    //     <Autocomplete
-    //       getItemValue={(item) => item.label}
-    //       items={getSearchTags()}
-    //       renderItem={(item, isHighlighted) => (
-    //         <div
-    //           className={cn(
-    //             "px-2 py-1 rounded-md cursor-pointer",
-    //             isHighlighted
-    //               ? "bg-gray-100 dark:bg-gray-900"
-    //               : "bg-white dark:bg-gray-800"
-    //           )}
-    //         >
-    //           {item.label}
-    //         </div>
-    //       )}
-    //       value={inputValue}
-    //       onChange={(e) => setInputValue(e.target.value)}
-    //       onSelect={(val) => setInputValue(val)}
-    //     />
-    //   </form>
-    // </div>
     <Autocomplete
       getItemValue={(item) => item.label}
-      items={getSearchTags()}
+      items={getSearchTags().filter((tag) => {
+        return tag.label.toLowerCase().includes(inputValue.toLowerCase());
+      })}
       renderMenu={(items, value, style) => (
         <div
-          className="absolute z-10 mt-3 max-w-3xl w-[300px] border border-border/70 bg-background rounded-md shadow-lg"
+          className="absolute z-10 mt-3 max-w-3xl w-[300px] border border-border/45 bg-popover/95 backdrop-blur-2xl backdrop-brightness-200  rounded-md shadow-lg"
           style={{
             ...style,
           }}
         >
           {items}
+
+          {items.length === 0 && (
+            <div className="p-2 text-center text-gray-500 dark:text-gray-400">
+              <RabbitIcon className="w-6 h-6 pt-1 mx-auto" />
+              <p className="text-sm pt-2">No results found</p>
+            </div>
+          )}
         </div>
       )}
       renderInput={(props) => (
-        // <div className="relative flex items-center bg-popover p-2 border-border/40 ">
-        //   <SearchIcon className=" text-gray-500 dark:text-gray-400" />
-        //   <Input
-        //     {...props}
-        //     className="w-full rounded-md  p-2 border-none focus-visible:ring-0 focus-visible:border-none bg-popover ring-0  text-sm"
-        //     placeholder="Search blog posts..."
-        //   />
-        // </div>
-        <div className="relative h-10 w-full">
-          <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10" />
+        <div className="relative h-10 w-full cursor-pointer">
+          <SearchIcon
+            onClick={() => {
+              if (inputValue) {
+                const query = new URLSearchParams();
+                query.set("q", inputValue);
+                router.push(`/blog/search?${query.toString()}`);
+              }
+            }}
+            className="absolute cursor-pointer bg-primary/30 hover:scale-105 transition-transform duration-150 text-primary rounded-md p-1 left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10"
+          />
           <Input
             {...props}
             type="text"
@@ -89,16 +57,34 @@ const SearchForm = () => {
       renderItem={(item, isHighlighted) => (
         <div
           className={cn(
-            "px-2 py-1 m-1 rounded-md cursor-pointer border-b border-border/70",
+            "px-2 py-1 m-1 rounded-md cursor-pointer flex  border-b border-border/70",
             isHighlighted && "bg-gray-100 dark:bg-gray-500/30"
           )}
         >
-          {item.label}
+          {Array.from(item.label as string).map((char, index) => {
+            return (
+              <p key={index}>
+                <span
+                  className={cn(
+                    inputValue.toLowerCase().includes(char.toLowerCase()) &&
+                      "text-primary"
+                  )}
+                >
+                  {char}
+                </span>
+              </p>
+            );
+          })}
         </div>
       )}
       value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
-      onSelect={(val) => setInputValue(val)}
+      onSelect={(val) => {
+        setInputValue(val);
+        const query = new URLSearchParams();
+        query.set("q", val);
+        router.push(`/blog/search?${query.toString()}`);
+      }}
     />
   );
 };
